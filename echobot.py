365fe2507c123084ceafa525b7b89da4e919d31d
jlwoo29 <a01084815968@gmail.com>
2026-04-01 18:33:14 +0900

    에코봇이 말을 따라하게 만듦
    입력창에 내가 입력한 값을 따라하게 만들었다. 대화 목록도 있음

--- echobot.py
+++ echobot.py
@@ -1,11 +1,32 @@
 import streamlit as st
 
-st.title(":red[:material/smart_toy:] 에코봇")
+st.title(":orange[:material/smart_toy:] 에코봇")
 
-if prompt :st.chat_input('에코봇에게 물어보기'):
+st.caption("내 말을 따라하는 따라쟁이입니다.")
 
-    with st.chat_message('user'):
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+
+if prompt := st.chat_input("따라쟁이에게 물어보기"):
+    with st.chat_message("user"):
         st.write(prompt)
+        message = {
+            "role": "user",
+            "content": prompt
+        }
+        st.session_state.messages.append(message)
 
-    with st.chat_message('ai'):
+    with st.chat_message("ai"):
         st.write(prompt)
+        message = {
+            "role": "ai",
+            "content": prompt
+        }
+        st.session_state.messages.append(message)
+
