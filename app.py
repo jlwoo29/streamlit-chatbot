27d1874bb4db739d705941e4e311f1c1820745e5
jlwoo29 <a01084815968@gmail.com>
2026-04-06 17:27:45 +0900

    도구를 사용할 수 있음
    색 추천&변경도 가능하고 심박수도 알려줌

--- app.py
+++ app.py
@@ -22,6 +22,31 @@ def get_client():
 
 client = get_client()
 
+if "box_color" not in st.session_state:
+    st.session_state.box_color = "#FFFFFF"
+
+def change_color(hex_code: str) -> dict:
+    """
+    이 함수는 사이드바에 있는 컬러 박스의 색상을 변경합니다.
+    
+    Args:
+        hex_code (str): 색상의 HEX 코드 (예: "#87CEEB")
+    """
+    st.session_state.box_color = hex_code
+
+    return {
+        "status": "success",
+        "message": f"{hex_code}으로 색상이 변경되었습니다."
+    }
+
+def get_heartbeat() -> int:
+    """
+    내 심박수를 반환합니다.
+
+    return 심박수
+    """
+    return 100
+
 @st.cache_data
 def load_system_prompt(filename):
     if os.path.exists(filename):
@@ -38,14 +63,18 @@ if st.sidebar.button("🔄 대화 및 설정 초기화"):
     st.rerun()
 
 # 시스템 프롬프트 로드
-system_prompt = load_system_prompt('system_prompt.md')
 
 if "chat_session" not in st.session_state:
+    system_prompt = load_system_prompt('system_prompt.md')
+
     st.session_state.chat_session = client.chats.create(
         model=MODEL_NAME,
         config=types.GenerateContentConfig(
             system_instruction=system_prompt,
-            temperature=0.2, # 값이 낮을수록 지시사항을 더 잘 지킵니다.
+            tools=[change_color, get_heartbeat], 
+            automatic_function_calling=types.AutomaticFunctionCallingConfig(
+                disable=False
+            )
         )
     )
 
@@ -56,7 +85,12 @@ for content in st.session_state.chat_session.get_history():
         for part in content.parts:
             if part.text:
                 st.write(part.text)
-
+            if part.function_call:
+                with st.status(f"{part.function_call.name} 함수 호출 요청"):
+                    st.json(part.function_call.args)
+            if part.function_response:
+                with st.status(f"{part.function_response.name} 함수 실행 완료"):
+                    st.json(part.function_response.response)
 
 # 채팅 입력
 if prompt := st.chat_input("서영이에게 물어보기"):
@@ -70,3 +104,22 @@ if prompt := st.chat_input("서영이에게 물어보기"):
 
         response = st.session_state.chat_session.send_message(prompt)
         st.write(response.text)
+        st.rerun()
+
+with st.sidebar:
+    st.header("팔레트")
+    st.markdown(f"""
+        <div
+            style="background-color: {st.session_state.box_color};
+            height: 150px;
+            border-radius: 15px; 
+            border: 2px solid #ddd;
+            box-shadow: 2px 2px 10px rgba(0,0,0,0.05);
+            transition: background-color 0.5s ease;
+        ">
+        </div>
+    """, unsafe_allow_html=True)
+
+if st.sidebar.button('색상 변경'):
+        change_color('#c4a0db')
+        st.rerun()
